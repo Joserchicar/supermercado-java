0c2ba0349300fc3bbb4c4ee89cc66d62e4e79030
javaee <javaee@javaee-VirtualBox>
2020-10-07 09:12:01 +0200

    cambio final

--- supermercado-java/src/main/java/com/ipartek/formacion/ejemplos/modelo/ActividadEjemploDAO.java
+++ supermercado-java/src/main/java/com/ipartek/formacion/ejemplos/modelo/ActividadEjemploDAO.java
@@ -1,18 +1,23 @@
+
 package com.ipartek.formacion.ejemplos.modelo;
 
 import java.util.List;
+
+/**
+ * Ejempo de DAO de un producto con dos funciones
+ * 
+ * {@code List<Producto> listarProductos()}
+	
+	{@code List<String> listarCategoriasProductos()}
+	
+ */
 
 import com.ipartek.formacion.modelo.pojo.Producto;
 
 public interface ActividadEjemploDAO {
 
-	
 	List<Producto> listarProductos();
-	
+
 	List<String> listarCategoriasProductos();
-	
-	
-	
-	
-	
+
 }
